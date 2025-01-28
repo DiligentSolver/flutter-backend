51a36f3378bffbe3a6ede0fa9310e79346b6adab
DiligentSolver <trialuse87@gmail.com>
2025-01-28 15:42:50 +0530

    minor changes

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use("/api/onboarding", onboardingRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 app.get("/", (req, res) => {
