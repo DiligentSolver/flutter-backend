94c808ab1f36360fb0845cc0d4cb79a47a9810d7
DiligentSolver <trialuse87@gmail.com>
2025-01-30 12:56:59 +0530

    updated-url

--- routes/onboardingRoutes.js
+++ routes/onboardingRoutes.js
@@ -5,8 +5,7 @@ const router = express.Router();
 router.get("/video", (req, res) => {
   try {
     // Construct the URL of the video hosted on the API server
-    const videoUrl =
-      "https://ik.imagekit.io/zf8c9okck/onboarding_video_final1.mp4"; // Build the complete video URL
+    const videoUrl = "https://ik.imagekit.io/zf8c9okck/onboard.mp4"; // Build the complete video URL
     res.status(200).json({ videoUrl });
   } catch (err) {
     res.status(500).json({ error: "Failed to fetch video URL" });
